a893ecef1448602704240cc42d7b27bcf1e5e89f
Jelgar <jonathanelgardiner@gmail.com>
2016-04-13 16:06:49 +0100

    testing added

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+var fortune = require('./lib/fortune.js');
 //set up handlebars view engine
 var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
@@ -8,13 +9,33 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
-var fortune = require('./lib/fortune.js');
+app.use(function(req, res, next){
+  res.locals.showTests = app.get('env') !== 'production' &&
+  req.query.test === '1';
+  next();
+});
+
 app.get('/', function(req, res){
   res.render('home');
 });
 
 app.get('/about', function(req, res){
-  res.render('about', {fortune: fortune.getFortune});
+  res.render('about', {
+    fortune: fortune.getFortune(),
+    pageTestScript: 'qa/tests-about.js'
+  } );
+});
+
+app.get('/tours/hood-river', function(req, res){
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/oregon-coast', function(req, res){
+  res.render('tours/oregon-coast');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+  res.render('tours/request-group-rate');
 });
 
 // custom 404 page
