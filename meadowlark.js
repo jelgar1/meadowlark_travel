d6c5b19e2d7364b473a7f4bef78c3cabde3b45e5
Jelgar <jonathanelgardiner@gmail.com>
2016-04-14 10:34:04 +0100

    added sections

--- meadowlark.js
+++ meadowlark.js
@@ -3,7 +3,16 @@ var app = express();
 
 var fortune = require('./lib/fortune.js');
 //set up handlebars view engine
-var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express3-handlebars').create({
+  defaultLayout:'main',
+  helpers: {
+    section: function(name, options){
+      if(!this._sections) this.sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
@@ -12,6 +21,42 @@ app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next){
   res.locals.showTests = app.get('env') !== 'production' &&
   req.query.test === '1';
+  next();
+});
+
+// mocked weather data
+var getWeatherData = function(){
+  return {
+    locations: [
+      {
+        name: 'Portland',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Overcast',
+        temp: '54.1 F (12.3 C)',
+      },
+      {
+        name: 'Bend',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Partly Cloudy',
+        temp: '55.0 F (12.8 C)',
+      },
+      {
+        name: 'Manzanita',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Light Rain',
+        temp: '55.0 F (12.8 C)',
+      },
+    ],
+  };
+}
+
+// middleware to add weather data to context
+app.use(function(req, res, next){
+  if(!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weather = getWeatherData();
   next();
 });
 
